b766a021558221b6a724e8c4ef20e791da131111
Chrono4 <kanemiles445@gmail.com>
2020-01-19 12:17:00 +0000

    test commit

--- app/src/main/java/com/example/tracktrack/MainActivity.kt
+++ app/src/main/java/com/example/tracktrack/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) //test change
 
         //Gets UI elements.
         val currentBtn = findViewById<Button>(R.id.currentButton)
