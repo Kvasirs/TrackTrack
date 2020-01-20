eff85aa6f2d79b58ca445b2e9cbceab461d59409
Chrono4 <kanemiles445@gmail.com>
2020-01-20 19:49:27 +0000

    Added Jsoup library import.

--- app/src/main/java/com/example/tracktrack/MainActivity.kt
+++ app/src/main/java/com/example/tracktrack/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import androidx.core.content.ContextCompat
+import org.jsoup.Jsoup
 
 /**
  * MainActivity - Handles events made on the main menu. Will allow
